f6d06e446e3c531ab5ad4715ccaa49456b24c5dc
0DAC <mzhang21@lawrenceville.org>
2019-10-01 10:30:11 -0400

    Added functionality: Resuming generation evolution from existing generation history

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 import population as pop
 import gamemaster as gm
+import os, os.path
 import random as r
 import argparse
 
@@ -13,8 +14,18 @@ if __name__ == '__main__':
     parser.add_argument("-s", "--save", type=str, help="Directory to save the evolved generation map")
     args = vars(parser.parse_args())
 
+    resume = False
+    try:
+        if args['save']:
+            os.makedirs("./" + args['save'])
+    except FileExistsError:
+        resume = input("Resume Training? (Y/N)\n>>> ")
+
     mp = gm.GameMaster('MountainCarContinuous-v0', thread_num=args['threadcount'])
-    crowd = pop.Population(args['popsize'], 1, 1, evaluator=mp)
+    crowd = pop.Population(0 if resume else args['popsize'], 1, 1, evaluator=mp)
+    if resume:
+        latest_gen = len([name for name in os.listdir("./" + args['save']) if os.path.isfile(name)])
+        crowd.load_from_file("./" + args['save'] + "/gen-" + str(latest_gen+1) + ".txt")
 
     # norm_fitness = crowd.get_normalized_fitness()
     # mating_pool = crowd.get_mating_pool(norm_fitness)
